63ae3d3b974d8446fa4c198ad3971e582bab38b4
Eduardo Fagundes Leal <eduardofagundesleal@gmail.com>
2022-03-22 14:37:49 -0300

    Criação de telas

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControDeContatos.Controllers
+{
+    public class ContatoController
+    {
+    }
+}
---
+++ Controllers/ContatoController2.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControDeContatos.Controllers
+{
+    public class ContatoController2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
